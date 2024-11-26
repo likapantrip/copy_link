bb3529dc993743627b55f7c28312398639eb23a8
likapantrip <likapantrip@gmail.com>
2024-11-26 21:34:12 +0900

    エラー記述を修正

--- app/javascript/copy_link.js
+++ app/javascript/copy_link.js
@@ -5,26 +5,26 @@ function copy_link (){
   // コピーするリンクを取得
   const copyLink = document.getElementById('copy-link').innerHTML;
   
-  if (copyIcon) {
-    copyIcon.addEventListener('click', (event) => {
-      // クリップボードにコピー
-      navigator.clipboard.writeText(copyLink)
-        .then(() => {
-          // メッセージを取得
-          const copyMessage = document.getElementById('copy-message');
+  if(copyIcon){
+    copyIcon.addEventListener('click', () => {
+      try{
+        // クリップボードにコピー
+        navigator.clipboard.writeText(copyLink)
 
-          // 成功時の処理
-          if (copyMessage) {
-            copyMessage.style.display = 'block';
-            setTimeout(() => {
-              copyMessage.style.display = 'none';
-            }, 2000); // メッセージを2秒後に非表示
-          }
-        })
-        .catch((err) => {
-          alert('URLリンクのコピーに失敗しました');
-        });
-    });
+        // メッセージを取得
+        const copyMessage = document.getElementById('copy-message');
+
+        // メッセージを表示
+        if (copyMessage) {
+          copyMessage.style.display = 'block';
+          setTimeout(() => {
+            copyMessage.style.display = 'none';
+          }, 2000); // メッセージを2秒後に非表示
+        }
+      }catch{
+        alert('URLリンクのコピーに失敗しました');
+      }
+    })
   }
 };
 
